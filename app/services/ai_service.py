135943f1aafc4fbf5546f0c6af8219402bab6436
kangdeagyu <rkdeorb0124@naver.com>
2023-10-18 12:36:45 +0900

    이미지 전처리 변경 및 gitignore 변경

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -8,14 +8,19 @@ from PIL import Image
 
 class AI_Service:
     def __init__(self):
-        self.model = load_model('app/static/best_face_cnn_model.h5')
+        self.model = load_model('app/static/64-256_256-2.h5')
         # Dlib 모델 불러오기
         self.face_cascade = dlib.get_frontal_face_detector()
 
     def AI_predict(self, image_file, target_size=(128, 128), padding_size=(182, 182)):
         try:
-            # BGR GRAY 색상 변환
-            gray = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+            # 이미지 불러오기 (Image -> Numpy Array)
+            img_pil = Image.open(image_file)
+            img = np.array(img_pil)
+
+            # BGR GRAY 색상 변환 (PIL은 RGB 순서이므로 RGB->BGR 변환 후 GRAY 색상 변환)
+            img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
             # 얼굴 찾기 
             rects = self.face_cascade(gray)
@@ -40,7 +45,7 @@ class AI_Service:
 
             # 학습된 데이터와 맞게 패딩 설정
             padded_face=np.zeros((padding_size[1],padding_size[0],3),dtype=np.uint8)
-        
+    
             x_offset=(padding_size[0]-target_size[0])//2
             y_offset=(padding_size[1]-target_size[1])//2
 
@@ -49,23 +54,18 @@ class AI_Service:
             # 그레이스케일 변경
             padded_face_gray=cv2.cvtColor(padded_face,cv2.COLOR_BGR2GRAY)
 
-            # 데이터 프레임 형태로 변환
-            train=np.zeros(1*182*182*1,dtype=np.int32).reshape(1,182,182) 
-
-            img=np.array(padded_face_gray,dtype=np.int32)
-            train[0,:,:]=img
-
-            train=train.reshape(-1,182 * 182)
+            # 데이터 프레임 형태로 변환             
+            train=padded_face_gray.reshape(1,padded_face_gray.shape[0]*padded_face_gray.shape[1])
 
             df=pd.DataFrame(train)
 
-            width, height, channel = 182, 182, 1 # 이미지 사이즈 182*182 pixel
+            width, height, channel=182,182 ,1# 이미지 사이즈 182*182 pixel
 
-            x_train = df.values
-            x_train = x_train.reshape((x_train.shape[0], width, height, channel))
+            x_train=df.values
+        
+            x_train=x_train.reshape((x_train.shape [0],width,height ,channel))
 
-            # Image Pixel Normalization [0,255] -> [-1,1] , tanh를 사용키 위해 tanh는 -1에서 1까지의 값
-            X=(x_train-127.5)/127.5
+            X=(x_train-127.5)/127.5 
 
             pred=self.model.predict(X)
 
@@ -75,4 +75,4 @@ class AI_Service:
         except Exception as e:
             print('error:',e)
             err=['인식오류']
-            return err
+            return err  
