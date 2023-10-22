2a26d8b689c582602e5a245dd3182258d324b642
kangdeagyu <rkdeorb0124@naver.com>
2023-10-22 19:07:06 +0900

    이미지 라이브러리 변경 및 이미지 전처리 수정

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,18 +1,24 @@
 import cv2
 from keras.models import load_model
 import numpy as np
-from flask import Flask
-import dlib
 import pandas as pd
 from PIL import Image
 
 class AI_Service:
-    def __init__(self):
-        self.model = load_model('app/static/64-256_256-2.h5')
-        # Dlib 모델 불러오기
-        self.face_cascade = dlib.get_frontal_face_detector()
+    def __init__(self,
+                    model_path='app/static/16-16_16_cnn_model.h5',
+                    cascade_path="app/static/haarcascade_frontalface_default.xml",
+                    target_size=(128, 128),
+                    padding_size=(184, 184),
+                    brightness_adjustment=83.15755552578428):
+        self.model = load_model(model_path)
+        # openCV xml 불러오기
+        self.face_cascade = cv2.CascadeClassifier(cascade_path)
+        self.target_size = target_size
+        self.padding_size = padding_size
+        self.brightness_adjustment = brightness_adjustment
 
-    def AI_predict(self, image_file, target_size=(128, 128), padding_size=(182, 182)):
+    def AI_predict(self, image_file):
         try:
             # 이미지 불러오기 (Image -> Numpy Array)
             img_pil = Image.open(image_file)
@@ -20,59 +26,71 @@ class AI_Service:
 
             # BGR GRAY 색상 변환 (PIL은 RGB 순서이므로 RGB->BGR 변환 후 GRAY 색상 변환)
             img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
-            # 얼굴 찾기 
-            rects = self.face_cascade(gray)
+            faces = self.face_cascade.detectMultiScale(img_bgr,scaleFactor=1.1,minNeighbors=5)
 
-            for rect in rects:
-                x = rect.left()
-                y = rect.top()
-                w = rect.width()
-                h = rect.height()
+            # 차원 정리 -> 1차원
+            faces = faces.reshape(-1)
 
-                # 얼굴 영역 자르기 
-                face_crop = img[y:y+h,x:x+w]
+            imgCrop = img_bgr[faces[1]:faces[1]+faces[3],faces[0]:faces[0]+faces[2]]
 
-                # 잘라낸 면의 크기를 목표 크기로 조정 
-                face_resized=cv2.resize(face_crop,target_size)
+            # 잘린 이미지 사이즈 조절 (128,128)
+            face_resized=cv2.resize(imgCrop,self.target_size)
 
-            # 학습된 데이터와 맞게 밝기 설정
-            adjusted_image = face_resized.astype(float) - np.mean(face_resized) + 133.41081377158957
+            # 패딩 사이즈 설정
+            padded_face=np.zeros((*self.padding_size,3),dtype=np.uint8)
+                
+            x_offset=(self.padding_size[0]-self.target_size[0])//2
+            y_offset=(self.padding_size[1]-self.target_size[1])//2
 
-            adjusted_image[adjusted_image < 0] = 0   # 음수 값은 0으로 설정
-            adjusted_image[adjusted_image > 255] = 255   # 초과하는 값은 최댓값인 255로 설정
+        except Exception as e:
+            print('error in image processing:',e)
+            return '인식오류'
 
-            # 학습된 데이터와 맞게 패딩 설정
-            padded_face=np.zeros((padding_size[1],padding_size[0],3),dtype=np.uint8)
-    
-            x_offset=(padding_size[0]-target_size[0])//2
-            y_offset=(padding_size[1]-target_size[1])//2
+        try:
+            padded_face[y_offset:y_offset+self.target_size[1],x_offset:x_offset+self.target_size[0]]=face_resized
 
-            padded_face[y_offset:y_offset+target_size[1],x_offset:x_offset+target_size[0]]=adjusted_image
+            # 학습된 이미지 평균 밝기로 밝기 조절
+            adjusted_image = padded_face.astype(float) - np.mean(padded_face) + self.brightness_adjustment
 
-            # 그레이스케일 변경
-            padded_face_gray=cv2.cvtColor(padded_face,cv2.COLOR_BGR2GRAY)
+            adjusted_image = np.clip(adjusted_image, 0, 255)   # 음수 값은 0으로 설정, 초과하는 값은 최댓값인 255로 설정
 
-            # 데이터 프레임 형태로 변환             
-            train=padded_face_gray.reshape(1,padded_face_gray.shape[0]*padded_face_gray.shape[1])
+        except Exception as e:
+            print('error in image adjustment:',e)
+            return '인식오류'
+
+        try:  
+            img2 = Image.fromarray(adjusted_image.astype('uint8'))
+            img2 = img2.convert('L')
+
+            train=np.zeros(1*184*184,dtype=np.int32).reshape(1,184,184) 
+
+            img=np.array(img2,dtype=np.int32)
+            train[0,:,:]=img
+
+            train=train.reshape(-1,184 * 184)
 
             df=pd.DataFrame(train)
 
-            width, height, channel=182,182 ,1# 이미지 사이즈 182*182 pixel
+        except Exception as e:
+            print('error in converting image to grayscale:',e)
+            return '인식오류'
+
+        try: 
+            width, height, channel=184 ,184 ,1 # 이미지 사이즈 184*184 pixel
 
             x_train=df.values
-        
+            
             x_train=x_train.reshape((x_train.shape [0],width,height ,channel))
 
             X=(x_train-127.5)/127.5 
 
             pred=self.model.predict(X)
 
-            classes=['10대','20대','30대','40대','50대','60대','70대']
-            
-            return classes[np.argmax(pred)]
         except Exception as e:
-            print('error:',e)
-            err=['인식오류']
-            return err  
+            print('error in model prediction:',e)
+            return '인식오류'
+            
+        classes=['10대','20대','30대','40대','50대','60대','70대']
+                
+        return classes[np.argmax(pred)]
